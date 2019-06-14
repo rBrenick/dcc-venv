89eeff571435f322b5eb54eaf1fe04207bb96a6e
Richard Brenick <RichardBrenick@gmail.com>
2019-06-14 22:32:35 +0200

    modified startup to use default lib 'site'

--- dcc_venv/common/dcc_venv_startup.py
+++ dcc_venv/common/dcc_venv_startup.py
@@ -1,46 +1,19 @@
 import os
 import sys
+import site
 import inspect
 
 def startup():
 
     sys.stdout.write("#"*50)
-    sys.stdout.write("\n dcc_venv setup\n")
-    
+    sys.stdout.write("\ndcc_venv_startup\n")
     
     # Add site-packages to sys.path
     CURRENT_DIR = os.path.dirname(inspect.getfile(inspect.currentframe()))
     
-    if CURRENT_DIR not in sys.path:
-        sys.path.append(CURRENT_DIR)
-    
-    
-    
-    # Adding Egg Links that might be in site-packages
-    site_paths = []
-    for f in os.listdir(CURRENT_DIR):
-        if f.endswith('.egg-link'):
+    if CURRENT_DIR not in site.getsitepackages():
+        site.addsitedir(CURRENT_DIR)
         
-            egg_link_path = os.path.join(CURRENT_DIR, f)
-            
-            with open(egg_link_path, "r") as fp:
-                for path in fp.read().splitlines():
-                    if path == ".":
-                        continue
-                        
-                    site_paths.append(os.path.abspath(path))
-
-    if site_paths:
-        sys.stdout.write("\nDEV site-setup\n")
-                        
-        for site_path in site_paths:
-            if not site_path in sys.path:
-                print("DEV - adding to sys.path {}".format(site_path))
-                sys.path.append(site_path)
-            
-            
-            
-            
     # startup complete
     sys.stdout.write("#"*50)
 
